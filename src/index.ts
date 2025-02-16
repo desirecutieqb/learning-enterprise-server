ee1db7ce5026b3ff9829bbb2f42b9157f9c76fc3
0QKub0 <qkubplay@gmail.com>
2025-02-16 20:35:04 +0100

    added clerk routes

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,18 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
-
+import {createClerkClient} from "@clerk/express";
 import courseRoutes from "./routes/courseRoutes";
+import userClerkRoutes from "./routes/userClerkRoutes"
 // CONFIGURATIONS
 dotenv.config();
 const isProduction = process.env.NODE_ENV === "production";
 if(!isProduction){
     dynamoose.aws.ddb.local();
 }
+export const clerkClient = createClerkClient({
+    secretKey: process.env.CLERK_SECRET_KEY
+})
 
 const app = express();
 app.use(express.json());
@@ -29,6 +33,7 @@ app.get("/",(req,res)=>{
 })
 
 app.use("/courses",courseRoutes);
+app.use("/users/clerk",userClerkRoutes);
 
 //SERVER
 
